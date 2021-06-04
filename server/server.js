7f7df848b88b53d39a59a1dac0a9e3952100e9f1
Gagandeep Singh <gagan.ddun@gmail.com>
2021-06-04 22:50:30 +1000

    feat: adds graphql and apollo-server-express

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import { ApolloServer, gql } from 'apollo-server-express';
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
@@ -9,20 +10,30 @@ const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false,
+  })
+);
+
+const typeDefs = gql``;
+
+const resolvers = {};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   const user = db.users.list().find((user) => user.email === email);
   if (!(user && user.password === password)) {
     res.sendStatus(401);
     return;
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+  const token = jwt.sign({ sub: user.id }, jwtSecret);
+  res.send({ token });
 });
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
